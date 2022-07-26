e2be41ed1c532adff2395b79ef97ba5e5fc40f49
Slava <slavavd.didenko35@gmail.com>
2022-07-27 01:10:44 +0300

    fixed menu burger and custom header

--- js/script.js
+++ js/script.js
@@ -1,14 +1,51 @@
-document.querySelector('.header__burger').onclick = () => {
-  document.getElementsByTagName('body')[0].classList.add('hidden');
-  document.querySelector('.header__menu-wrapper').classList.add('active-menu');
-  document.querySelector('.header').classList.add('back');
-}
-document.querySelector('.header__close-btn').onclick = () => {
-  document.querySelector('.header__menu-wrapper').classList.remove('active-menu');
-  document.getElementsByTagName('body')[0].classList.remove('hidden');
-  document.querySelector('.header').classList.remove('back');
+const menu = document.querySelector('.header__menu-wrapper');
+const burger = document.querySelector('.header__burger');
+const body = document.getElementsByTagName('body')[0].classList;
+const header = document.querySelector('.header');
+const closeBurger = document.querySelector('.header__close-btn');
+const headerHeight = header.offsetHeight;
+const hero = document.querySelector('.main-page');
+const heightHero = hero.offsetHeight;
+let lastScrollTop = 0;
+
+window.addEventListener('scroll', () => {
+  let scrollDistance = window.scrollY;
+
+  if (scrollDistance >= heightHero) {
+    header.style.transform = `translateY(0)`;
+    header.classList.add('header-fixed')
+    // if (lastScrollTop < scrollDistance && scrollDistance >= heightHero + headerHeight) {
+    //   header.classList.remove('header-fixed')
+    //   lastScrollTop = scrollDistance;
+    // } else {
+    //   header.classList.remove('header-fixed')
+    // }
+  } else {
+    header.classList.remove('header-fixed')
+  }
+})
+
+function offMenuBurger() {
+  menu.classList.remove('active-menu');
+  body.remove('hidden');
+  header.classList.remove('back');
 }
 
+burger.onclick = () => {
+  body.add('hidden');
+  menu.classList.add('active-menu');
+  header.classList.add('back');
+}
+
+closeBurger.onclick = () => {
+  offMenuBurger();
+}
+
+document.addEventListener('click', (e) => {
+  if (!e.target.closest('.active-menu') && menu.className.includes('active-menu') && !e.target.closest('.header__burger')) {
+    offMenuBurger();
+  }
+})
 
 const swiper = new Swiper('.swiper', {
   slidesPerView: 2,
@@ -28,7 +65,4 @@ const swiper = new Swiper('.swiper', {
   },
 });
 
-const swipers = document.querySelector('.swiper').swiper;
-
-// Now you can use all slider methods like
 swiper.slideNext();
